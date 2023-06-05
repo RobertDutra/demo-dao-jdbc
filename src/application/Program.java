cda12e6da4c90d447631a0cfbd6cd505de4c7b32
Robert Dutra <robertdutra10@hotmail.com>
2023-06-05 19:21:13 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -41,6 +41,10 @@ public class Program {
 		seller.setName("Martha Waine");
 		sellerDao.update(seller);
 		System.out.println("Update completed");
+		
+		System.out.println("\n=== TEST 6: seller delete =====");
+		sellerDao.deletById(11);
+		System.out.println("Delete completed");
 
 	}
 
